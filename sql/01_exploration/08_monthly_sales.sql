745ff35773ae6bbbf054698e6294810f75d6aff6
Maximiliano Cóceres <maximiliano.g.coceres@gmail.com>
2026-05-22 15:34:51 +0200

    [8] Monthly Sales  (#16)
    * feat: I created te script of monthly sales.
    
    * fix: I fixed the script, I change the part of avg_order_value. The division should now be done with COUNT(DISTINCT o.orderNumber))
    
    * Fix: I fixed avg_order_value. I deleted the multiplicated for 100.
    
    * Fix: I fixed avg_order_value. I deleted the multiplicated for 100.

---
+++ sql/01_exploration/08_monthly_sales.sql
@@ -0,0 +1,35 @@
+-- ============================================
+-- Archivo: 08_monthly_sales.sql
+-- Autor:   Maximiliano G. Coceres
+-- Fecha:   2026-05-22
+-- Issue:   #8
+-- Objetivo: Serie temporal de ventas mensuales
+-- ============================================
+
+use classicmodels;
+
+SELECT DATE_FORMAT(o.orderDate, '%Y-%m') AS año_mes,
+SUM(od.quantityOrdered * od.priceEach) AS total_revenue,
+SUM(od.quantityOrdered) AS total_orders,
+ROUND((SUM(od.quantityOrdered * od.priceEach) / COUNT(DISTINCT o.orderNumber)),2) AS avg_order_value
+FROM orders o
+LEFT JOIN orderdetails od ON o.orderNumber = od.orderNumber
+WHERE o.status = 'Shipped'
+GROUP BY año_mes
+ORDER BY año_mes ASC;
+
+SELECT DATE_FORMAT(o.orderDate, '%Y-%m') AS año_mes,
+SUM(od.quantityOrdered * od.priceEach) AS total_revenue,
+SUM(od.quantityOrdered) AS total_orders,
+ROUND((SUM(od.quantityOrdered * od.priceEach) / COUNT(DISTINCT o.orderNumber)),2) AS avg_order_value
+FROM orders o
+LEFT JOIN orderdetails od ON o.orderNumber = od.orderNumber
+WHERE o.status = 'Shipped'
+GROUP BY año_mes
+ORDER BY total_orders DESC;
+
+
+-- CONCLUSIONES
+-- El mes pico de ventas es noviembre. En ambos años fue el mes con mayores ventas, el 2003 con 10574 y un promedio de '33277.98' y el 2004 con 10208 con un promedio de '30184.29'
+-- En noviembre se duplican las ventas. 
+-- Encontre una subida muy importante en el mes de diciembre ya que en el 2003, se habian vendido 2961 ordenes y en el 2004, 4808 ordenes. 
